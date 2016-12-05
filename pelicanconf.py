493420c05338a58b49799dac8aa278b381fa669b
Blue Telecom Consulting <pablo.manuel.garcia@blue-tc.com>
2016-12-05 18:26:44 +0100

    Latest theme changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,22 +19,39 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATH = './plugins'
 PLUGINS = ['ipynb.markup']
+
+THEME = 'theme'
+
+# Blogroll
+LINKS = (("A Telecom's Blog", 'http://bluetc.es/en/sharing-and-engaging/a-telecoms-blog/blogger/listings/pablo-garcia'),
+         )
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+MAIL = 'pablo.manuel.garcia@blue-tc.com'
+
+TWITTER_USER = 'PablomgcBlue'
+
+LINKEDIN_USER = 'pablomgc'
+
+ABOUT_IMAGE = "images/about.jpg"
+
+#ABOUT_LINK = ""
+
+ABOUT_TEXT = """As a physicist, I started my career at the Nuclear Physics Group in the Universidad Complutense de Madrid where I became interested in the technical part of the work, linked to computational physics and Linux systems management.
+In 2012 I left the academic world and I started working for BlueTC as an external consultant in Ericsson R&D. There I began to develop my activity mainly as a Python developer of automation systems and testing facilities for continuous integration. In December 2014 I joined BlueTC's Solutions area, where I started working on projects related to Openstack, NFV/SDN and integrating analytics solutions with traditional monitoring environments.
+"""
+
+DEFAULT_PAGINATION = 10
+
+DISPLAY_CATEGORIES_ON_MENU = True
--- publishconf.py
+++ publishconf.py
@@ -11,14 +11,15 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://ozroc.github.io'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
+DISPLAY_CATEGORIES_ON_MENU = True
